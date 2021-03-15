832c4b4c65bbfbcca0e87144e75fbebf83889e4d
Vigly <ROBOCONN@uat.edu>
2021-03-15 05:50:15 -0700

    Cleaned up main function and added usage case.

--- SockClient/Main.cpp
+++ SockClient/Main.cpp
@@ -1,28 +1,24 @@
 #include "Functions.h"
+#define MAX 128
 
-INT main() {
+int main(int argc, char* argv[]) {
 
-	PCHAR IPAddress;
-	int IPSize = 16; //15 + NULL terminator
-	IPAddress = (char*)malloc(sizeof(char) * IPSize);
-	printf("Please enter IPv4 address to connect to: ");
-	fgets(IPAddress, IPSize, stdin);
-	IPAddress[strcspn(IPAddress, "\n")] = 0;
+	if (argc <= 2) {
+		printf("Usage: SockClient.exe [IP Address] [Port]");
+		exit(1);
+	}
+	else {
 
-	PCHAR port;
-	int portSize = 6; //5 + NULL Terminator
-	port = (char*)malloc(sizeof(char) * portSize);
-	printf("Please enter port to connect to: ");
-	fgets(port, portSize, stdin);
-	port[strcspn(port, "\n")] = 0;
+		PCHAR IPAddress = argv[1];
+		PCHAR port = argv[2];
 
-	PCHAR message;
-	int messageSize = 51; //50 + NULL Terminator
-	message = (char*)malloc(sizeof(char) * messageSize);
-	printf("Please enter message to send: ");
-	fgets(message, messageSize, stdin);
-	message[strcspn(message, "\n")] = 0;
+		char message[MAX] = { 0 };
+		printf("Message to send: ");
+		fgets(message, MAX, stdin);
 
-	wsaCreate();
-	sockCreateAndSend(IPAddress, port, message); //Passes IP and Port
+		wsaCreate();
+		sockCreateAndSend(IPAddress, port, message);
+	}
+	WSACleanup();
+	return 0;
 }
